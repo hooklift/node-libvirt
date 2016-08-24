6543dc06e67990e979bf88483376230772f379fd
Damian Kaczmarek <rush@rushbase.net>
2016-08-24 16:56:46 -0500

    test(issue65): make test run in loop

--- test/manual/issue65.js
+++ test/manual/issue65.js
@@ -10,10 +10,20 @@ if (global.gc) setInterval(global.gc, 1);
 
 var xml = fixture('storage_volume.xml');
 var hv = new virt.Hypervisor('test:///default');
-return hv.connectAsync()
-  .then(function() { return hv.lookupStoragePoolByNameAsync('default-pool'); })
-  .then(function(pool) { return pool.createVolumeAsync(xml); })
-  .delay(2000)
-  .then(function() { return hv.disconnectAsync(); })
-  .then(function() { console.log('success'); })
-  .catch(function(err) { console.log('error: ', err); });
+function run() {
+  return hv.connectAsync()
+    .then(function() { return hv.lookupStoragePoolByNameAsync('default-pool'); })
+    .delay(100)
+    .then(function(pool) { return pool.createVolumeAsync(xml); })
+    .delay(100)
+    .then(function() { return hv.disconnectAsync(); })
+    .delay(100)
+    .then(function() { console.log('success'); })
+    .catch(function(err) { console.log('error: ', err); });
+}
+
+function runLoop() {
+  return run().delay(100).then(runLoop);
+}
+
+runLoop();
